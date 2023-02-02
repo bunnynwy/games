140a129f30bac96e666ec30152e8a998b6f808a6
bunnynwy <96903327+bunnynwy@users.noreply.github.com>
2023-02-02 19:54:50 +0700

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,16 @@
+if game.PlaceId == 11542692507 then
+    print ("Anime Souls Simulator")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/bunnynwy/games/main/animesouls"))()
+elseif game.PlaceId == 11430505281 then
+    print ("Anime Weapon Simulator")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/bunnynwy/games/main/animeweapon"))()
+elseif game.PlaceId == 6777872443 then
+    print ("Pixel Piece")
+local TeleportService = game:GetService("TeleportService")
+TeleportService:Teleport(9380307595)
+elseif game.PlaceId == 9380307595 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/bunnynwy/games/main/pixelpiece"))()
+    print ("Pixel Piece(East blue)")
+    else
+        print ("Game not found")
+end
