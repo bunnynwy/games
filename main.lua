a5f08339985357eeeca718d06662bb83a7a839e6
bunnynwy <96903327+bunnynwy@users.noreply.github.com>
2023-04-29 23:34:55 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -16,6 +16,8 @@ elseif game.PlaceId == 12604352060 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/bunnynwy/games/main/arcane_odyssey"))()
     elseif game.PlaceId == 12413786484 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/bunnynwy/games/main/animelost"))()
+     elseif game.PlaceId == 12004201923 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/bunnynwy/games/main/chestsim"))()
     else
         print ("Game not found")
 end
