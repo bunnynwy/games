1396e3135b38699cfb260be2ee7f5263ad66b0ee
bunnynwy <96903327+bunnynwy@users.noreply.github.com>
2023-06-17 08:28:32 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -7,8 +7,6 @@ elseif game.PlaceId == 11430505281 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/bunnynwy/games/main/animeweapon"))()
 elseif game.PlaceId == 6777872443 then
     print ("Pixel Piece")
-local TeleportService = game:GetService("TeleportService")
-TeleportService:Teleport(9380307595)
 elseif game.PlaceId == 9380307595 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/bunnynwy/games/main/pixelpiece"))()
     print ("Pixel Piece(East blue)")
